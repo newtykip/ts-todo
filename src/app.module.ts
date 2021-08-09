10fe8eb7396c4e530c82b431458df3c7158444df
newt <hi@newty.dev>
2021-08-09 21:46:25 +0100

    List todos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-    imports: [AuthModule],
+    imports: [AuthModule, TodoModule],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/todo/todo.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
+import { PrismaService } from 'src/prisma.service';
+
+@Module({
+    providers: [TodoService, PrismaService],
+    controllers: [TodoController],
+    exports: [TodoService],
+})
+export class TodoModule {}
