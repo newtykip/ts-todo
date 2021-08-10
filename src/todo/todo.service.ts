562d9540cd4278ae9b7a8365df4ffd1d394a2c01
newt <hi@newty.dev>
2021-08-10 17:30:23 +0100

    Update todos

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -13,6 +13,10 @@ export class TodoService {
 
     readonly logger = new Logger();
 
+    private async getUser(userId: number) {
+        return await this.prisma.user.findUnique({ where: { id: userId } });
+    }
+
     async getUserTodos(id: number) {
         return await this.prisma.todo.findMany({ where: { ownerId: id } });
     }
@@ -50,9 +54,7 @@ export class TodoService {
             },
         });
 
-        const user = await this.prisma.user.findUnique({
-            where: { id: userId },
-        });
+        const user = await this.getUser(todo.ownerId);
 
         this.logger.log(
             `Created todo with ID ${todo.id} for User ${user.username} (ID: ${user.id})`,
@@ -62,32 +64,45 @@ export class TodoService {
     }
 
     async deleteTodo(userId: number, todoId: string): Promise<Todo> {
-        const todo = await this.prisma.todo.findUnique({
-            where: { id: parseInt(todoId) },
-        });
-
-        if (!todo) {
-            throw new BadRequestException('The requested todo does not exist!');
-        }
-
-        if (todo.ownerId !== userId) {
-            throw new UnauthorizedException(
-                'The authenticated user does not have access to the requested todo!',
-            );
-        }
+        const todo = await this.getTodo(userId, todoId);
 
         const deletedTodo = await this.prisma.todo.delete({
             where: { id: todo.id },
         });
 
-        const user = await this.prisma.user.findUnique({
-            where: { id: userId },
-        });
+        const user = await this.getUser(deletedTodo.ownerId);
 
         this.logger.log(
             `Deleted todo with ID ${deletedTodo.id} for User ${user.username} (ID: ${user.id})`,
         );
 
         return deletedTodo;
+    }
+
+    async updateTodo(userId: number, todoId: string, newData: Partial<Todo>) {
+        const isEmpty = Object.values(newData).every(
+            (x) => x === null || x === '' || !x,
+        );
+
+        if (isEmpty) {
+            throw new BadRequestException(
+                'The body must contain some data to update the todo with!',
+            );
+        }
+
+        const todo = await this.getTodo(userId, todoId);
+
+        const updatedTodo = await this.prisma.todo.update({
+            where: { id: todo.id },
+            data: { ...newData },
+        });
+
+        const user = await this.getUser(updatedTodo.ownerId);
+
+        this.logger.log(
+            `Updated todo with ID ${updatedTodo.id} for User ${user.username} (ID: ${user.id})`,
+        );
+
+        return updatedTodo;
     }
 }
