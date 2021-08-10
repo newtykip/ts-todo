84fd0b31ef591411d3c7f9bbacd2f9c10adbc631
newt! <newtykins@outlook.com>
2021-08-10 17:30:23 +0100

    Update todos

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -4,6 +4,7 @@ import {
     Delete,
     Get,
     Param,
+    Patch,
     Post,
     Req,
     UseGuards,
@@ -14,20 +15,6 @@ import { TodoService } from './todo.service';
 @Controller('todo')
 export class TodoController {
     constructor(private todoService: TodoService) {}
-
-    @UseGuards(JwtAuthGuard)
-    @Get()
-    async listTodos(@Req() req) {
-        const todos = await this.todoService.getUserTodos(req.user.id);
-        return todos;
-    }
-
-    @UseGuards(JwtAuthGuard)
-    @Get('/:id')
-    async getTodo(@Req() req, @Param() params) {
-        const todo = await this.todoService.getTodo(req.user.id, params.id);
-        return todo;
-    }
 
     @UseGuards(JwtAuthGuard)
     @Post()
@@ -45,5 +32,34 @@ export class TodoController {
         );
 
         return deletedTodo;
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async listTodos(@Req() req) {
+        const todos = await this.todoService.getUserTodos(req.user.id);
+        return todos;
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/:id')
+    async getTodo(@Req() req, @Param() params) {
+        const todo = await this.todoService.getTodo(req.user.id, params.id);
+        return todo;
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('/:id')
+    async updateTodo(@Req() req, @Param() params, @Body() body) {
+        const updatedTodo = await this.todoService.updateTodo(
+            req.user.id,
+            params.id,
+            {
+                text: body.text,
+                status: body.status,
+            },
+        );
+
+        return updatedTodo;
     }
 }
